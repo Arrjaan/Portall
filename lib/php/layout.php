2015962b38ba2bce3a476ed61ce3da327443d831
Arjan Spoelstra <arjanspoelstra@live.nl>
2012-04-17 19:38:04 +0200

    Fixed Layout.

--- lib/php/layout.php
+++ lib/php/layout.php
@@ -182,16 +182,17 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <!--
+	
+	-->
 	<script src="/lib/layout/js/jquery.js"></script>
     <script src="/lib/layout/js/bootstrap-scrollspy.js"></script>
     <script src="/lib/layout/js/bootstrap-tab.js"></script>
     <script src="/lib/layout/js/bootstrap-tooltip.js"></script>
     <script src="/lib/layout/js/bootstrap-popover.js"></script>
     <script src="/lib/layout/js/bootstrap-button.js"></script>
-    <script src="/lib/layout/js/bootstrap-collapse.js"></script>
     <script src="/lib/layout/js/bootstrap-carousel.js"></script>
     <script src="/lib/layout/js/bootstrap-typeahead.js"></script>
-	-->
+	<script src="/lib/layout/js/bootstrap-collapse.js"></script>
 	<script src="/lib/layout/js/bootstrap-transition.js"></script>
     <script src="/lib/layout/js/bootstrap-modal.js"></script>
 	<script src="/lib/layout/js/bootstrap-alert.js"></script>
